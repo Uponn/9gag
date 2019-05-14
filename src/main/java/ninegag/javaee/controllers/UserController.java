88be3621869bccb111b85a3a672a3e25d9124e23
steliyannedev <steliyan.nedev@gmail.com>
2019-05-14 23:28:18 +0300

    Exceptions added

--- src/main/java/ninegag/javaee/controllers/UserController.java
+++ src/main/java/ninegag/javaee/controllers/UserController.java
@@ -5,15 +5,21 @@ import ninegag.javaee.models.dto.UserLoginDTO;
 import ninegag.javaee.models.dto.UserRegisterDTO;
 import ninegag.javaee.models.pojo.User;
 import ninegag.javaee.models.repositories.UserRepo;
+import ninegag.javaee.util.ResponseMsg;
+import ninegag.javaee.util.exceptions.AlreadyTakenException;
+import ninegag.javaee.util.exceptions.InvalidInputException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 
 @RestController
-public class UserController {
+public class UserController extends BaseController{
     private static final String EMAIL_VERIFICATION = "^([\\p{L}-\\.]+){1,64}@([\\w&&[^_]]+){2,255}.[a-z]{2,}$";
     private static final String PASSWORD_PATTERN = "((?=.*[a-z])(?=.*\\d)(?=.*[A-Z])(?=.*[@#$%!]).{8,40})";
 
@@ -23,7 +29,7 @@ public class UserController {
     BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
     @PostMapping(value = "/register")
-    public Object registerUser(@RequestBody UserRegisterDTO userRegisterDTO){
+    public Object registerUser(@RequestBody UserRegisterDTO userRegisterDTO) throws InvalidInputException {
         validateRegisterDetails(userRegisterDTO);
         if (!userRepo.existsByUsername(userRegisterDTO.getUsername()) && !userRepo.existsByEmail(userRegisterDTO.getEmail())){
             User user = new User(userRegisterDTO.getFullName(), userRegisterDTO.getUsername(),
@@ -31,36 +37,36 @@ public class UserController {
                     userRegisterDTO.getEmail(),
                     userRegisterDTO.getAge());
             userRepo.save(user);
-            return user;
+            return new ResponseMsg("Registration was successful", HttpStatus.OK.value(), LocalDateTime.now());
         }else {
-            //TODO throw username or email already taken exception
+            throw new AlreadyTakenException("Username or Email already taken.");
         }
-        return "Something went wrong";
     }
     @PostMapping(value = "/login")
-    public Object loginUser(@RequestBody UserLoginDTO userLoginDTO){
+    public Object loginUser(@RequestBody UserLoginDTO userLoginDTO) throws InvalidInputException{
         validateLoginDetails(userLoginDTO);
         if (userRepo.existsByUsername(userLoginDTO.getUsername())) {
             User dbUser = userRepo.getByUsername(userLoginDTO.getUsername());
             if (encoder.matches(userLoginDTO.getPassword(), dbUser.getPassword())){
+                //TODO add session
                 return dbUser;
             }
         }
         return "Something went wrong";
     }
 
-    private void validateLoginDetails(UserLoginDTO user) {
+    private void validateLoginDetails(UserLoginDTO user) throws InvalidInputException{
         String username = user.getUsername();
         String password = user.getPassword();
         if (username == null || username.isEmpty() || username.contains( " " )){
-            //TODO throw input exception
+            throw new InvalidInputException("The username field is empty, please enter an username.");
         }
         if (password == null || password.isEmpty() || password.contains( " " )){
-            //TODO throw input exception
+            throw new InvalidInputException("The password field is empty, please enter a password.");
         }
     }
 
-    private void validateRegisterDetails(UserRegisterDTO user) {
+    private void validateRegisterDetails(UserRegisterDTO user) throws InvalidInputException{
         String fullName = user.getFullName();
         String username = user.getUsername();
         String password = user.getPassword();
@@ -69,25 +75,33 @@ public class UserController {
         int age = user.getAge();
 
         if (fullName == null || fullName.isEmpty() || !fullName.contains( " " )) {
-            //TODO throw input exception
+            throw new InvalidInputException("You have to enter your Full Name.");
         }
         if (username == null || username.isEmpty() || username.contains( " " )){
-            //TODO throw input exception
+            throw new InvalidInputException("You have to enter an username.");
         }
         if (password == null || password.isEmpty() || password.contains( " " )){
-            //TODO throw input exception
+            throw new InvalidInputException("You have left the password field empty, please enter a password.");
         }
         if (!password.equals(confirmPassword)){
-            //TODO throw input exception
+            throw new InvalidInputException("The two passwords you've entered do not match, please double check this.");
         }
         if (!password.matches(PASSWORD_PATTERN)){
-            //TODO throw input exception
+            throw new InvalidInputException("We want our passwords to :\n" +
+                    "Be between 8 and 40 characters long\n" +
+                    "Contain at least one digit.\n" +
+                    "Contain at least one lower case character.\n" +
+                    "Contain at least one upper case character.\n" +
+                    "Contain at least on special character from [ @ # $ % ! . ].");
         }
-        if (email == null || email.isEmpty() || !email.matches( EMAIL_VERIFICATION )){
-            //TODO throw input exception
+        if (email == null || email.isEmpty()){
+            throw new InvalidInputException("The email fields are empty, please fill those and continue.");
+        }
+        if (!email.matches( EMAIL_VERIFICATION )){
+            throw new InvalidInputException("Please enter a valid email address.");
         }
         if (age <= 0 || age > 120){
-            //TODO throw input exception
+            throw new InvalidInputException("Please enter a valid age.");
         }
 
     }
