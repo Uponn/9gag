40310d5d3e571e7d154e585215b83b9507d288ca
steliyannedev <steliyan.nedev@gmail.com>
2019-05-15 23:19:16 +0300

    Session Manager added

--- src/main/java/ninegag/javaee/util/SessionManager.java
+++ src/main/java/ninegag/javaee/util/SessionManager.java
@@ -1,4 +1,24 @@
 package ninegag.javaee.util;
 
+import ninegag.javaee.models.dto.UserSessionDTO;
+
+import javax.servlet.http.HttpSession;
+
 public class SessionManager {
+    private static final String LOGGED = "logged";
+    private static final String USER = "user";
+
+    public static boolean isLogged(HttpSession session){
+        if (session.isNew()){
+            return false;
+        }
+        if (session.getAttribute(LOGGED) == null){
+            return false;
+        }
+        return true;
+    }
+    public static void logUser(HttpSession session, UserSessionDTO user){
+        session.setAttribute(LOGGED, true);
+        session.setAttribute(USER, user);
+    }
 }
